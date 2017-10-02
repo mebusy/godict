4b1c64add24889f739c357bfbcf7b77fd9d834b5
qibinyi <qibinyi@qibinyis-MacBook-Pro.local>
2017-10-02 13:04:06 +0800

    m

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 gomobile bind -target=ios github.com/mebusy/godict/dictUtils
 
-DEST_PATH="../../../dict2017/"
+DEST_PATH="../../../../../dict2017/"
 rm -rf ${DEST_PATH}*.framework
 mv -f *.framework ${DEST_PATH}
 
